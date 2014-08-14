ca7d00235871011d2212364cf99d29ea086de12b
Brandon Jackson <brandonscottjackson@gmail.com>
2014-08-14 15:27:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Make-Turtle
+===========
+
+Make-Turtle is a simple introduction to Python's turtle drawing environment. It
+opens and resizes an IDLE console and an instance of `Turtle.screen`. To run it
+make sure the `make-turtle` bash script is executable, and then call it from
+the command line.
+
---
+++ make-turtle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2.7
+
+"""
+make-turtle
+by Brandon Jackson
+"""
+
+# Import Libraries
+from turtle import *
+
+# Print Welcome Message
+print("===============================================================================")
+print("")
+print("                             WELCOME TO MAKE-TURTLE")
+print("")
+print("                       .---.           _....._")
+print("                      /  p  `\     .-""`:     :`\"-.")
+print("                      |__   - |  ,'     .     '    ',")
+print("                       ._>    \ /:      :     ;      :,")
+print("                        '-.    '\`.     .     :     '  \\")
+print("                           `.   | .'._.' '._.' '._.'.  |")
+print("                             `;-\.   :     :     '   '/,__,")
+print("                             .-'`'._ '     .     : _.'.__.'")
+print("                            ((((-'/ `\";--..:..--;\"` \\")
+print("                                .'   /           \   \\")
+print("                               ((((-'           ((((-'")
+print("")
+print("                                LET'S DRAW TOGETHER!")
+print("                     YOUR TURTLE CAN UNDERSTAND THESE COMMANDS:")
+print("")
+print("- forward")
+print("- backward")
+print("- right")
+print("- left")
+print("- penup")
+print("- pendown")
+print("")
+print("===============================================================================")
+
+# Setup: Make Screen Bigger, Change Shape to Turtle
+setup(0.5,1.0,600,0)
+shape("turtle")
+
+
+# Open Screen
+s = getscreen()
